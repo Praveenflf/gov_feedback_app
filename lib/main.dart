fb6aec1c95ec9d9cf6585f12d87f8848e4477257
Praveenflf <praveenrflf@gmail.com>
2025-06-12 01:38:13 +0530

    udpate env

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:provider/provider.dart';
 import 'notifiers/feedback_notifier.dart';
 import 'notifiers/complaint_notifier.dart';
 import 'screens/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
